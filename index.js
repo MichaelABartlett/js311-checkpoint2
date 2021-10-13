79f5ff7b39186f4887dc02d5598cb8eee47f3265
unknown <michael@machinistchoice.com>
2021-10-13 18:58:26 -0500

    saving before adding auth, it is all working

--- index.js
+++ index.js
@@ -2,13 +2,12 @@
 
 
 const express = require("express");
-
-let env = require("dotenv").config(); // it does not need to be a variable
-
 let app = express();
 
 // enable the application to be able to parse JSON bodies in post/put
 app.use(express.json());
+
+let env = require("dotenv").config(); // it does not need to be a variable
 
 require("./example/db");
 
@@ -16,20 +15,10 @@ require("./example/db");
 let port = process.env.PORT;
 
 // connect to static content
-app.use(express.static("./public")); // this is not in use yet
+app.use(express.static("./public"));
 
 let exampleRoute = require("./example/route");
 app.use(exampleRoute);
-
-// tesing Heroku *******************************************************
-
-app.get("/", function(req, res){
-    //res.send("Hello Heroku App this is new to me, The code is: "+ process.env.SuperSecretCode)
-    let thing = 'I think this means we are working';
-    res.send(thing);
-})
-
-
 
 // this is not in use anymore
 //const port = process.env.PORT || 4001; // setting the port we are using
