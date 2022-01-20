e47583c0387fc1045266c7686e0a25f0bfed5309
DESKTOP-AONL5AH\owner <michaelbartlett864@gmail.com>
2022-01-19 20:24:34 -0600

    adding frontend

---
+++ client/src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import React, { useState, useEffect} from "react";
+import Axios from 'axios';
+// import { addRecipe } from '../../controller/recipes';
+import {Work} from './components/inWork'
+
+
+function App() {
+
+const {recipeName, setRecipeName} = useState('')
+const {image, setImage} = useState('')
+const {servings, setServings} = useState('')
+
+const recipeNames = () => {
+
+}
+
+const addRecipe = (req, res) => {
+  Axios.post("/recipe/add", {
+    recipe_name: recipeName,
+    image: image,
+    servings: servings
+  }).then(() => {
+    alert("Successful insert!!!!!!!!!!!!!!!!!!!!!!!!!")
+  })
+}
+
+  return (
+    <div className="App">
+      <div>
+       < Work />
+        <h1>Working things out</h1>
+        <button onClick={recipeNames}>Get Recipe Name List</button>
+      </div>
+      <div>
+        <h1>Adding a New Recipe</h1>
+        <label>recipeName</label>
+        <input type="text" name="recipeName" onChange={(e) => {
+          setRecipeName(e.target.value)
+        }}></input>
+        <label>image</label>
+        <input type="text" name="image" onChange={(e) => {
+          setImage(e.target.value)
+        }}></input>
+        <label>servings</label>
+        <input type="text" name="servings" onChange={(e) => {
+          setServings(e.target.value)
+        }}></input>
+        <button onClick={addRecipe}>Add the new Recipe</button>
+      </div>
+    </div>
+
+  );
+}
+
+export default App;
---
+++ client/src/components/inWork.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useEffect, useState } from 'react'
+
+export const Work = () => {
+    return(
+    <div>inWork</div>
+    )
+}
