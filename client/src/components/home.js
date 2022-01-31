89f11dd545a5d3eb70625263f4f8c683ef9cd9fb
DESKTOP-AONL5AH\owner <michaelbartlett864@gmail.com>
2022-01-30 19:01:51 -0600

    try to send to heroku1

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -1,18 +1,12 @@
 
 import '../Stlyes/home.css'
-import React, { useState, useEffect} from "react";
-import Axios from 'axios';
+import React from "react";
 import hungryKid from '../images/hungryKid.jpg'
 import frozenChicken from '../images/frozenChicken.jpg'
 
 
 
 function Home() {
-
-  const ViewRecipe = () => {
-
-  }
-
 
   return (
     <main className="home">
@@ -21,9 +15,9 @@ function Home() {
         <h1>Cooking Prep</h1>
         <h1>Where we help</h1>
         <h1>You prep your food</h1>
-        <input type="text" name="userName" placeholder='User Name' className='input' onChange={(e) => {
+        {/* <input type="text" name="userName" placeholder='User Name' className='input' onChange={(e) => {
                 ViewRecipe(e.target.value)
-                }}></input>
+                }}></input> */}
         <h1>Don't let this be you!!</h1>
         <section className='images'>
           <div class="hungryKid">
