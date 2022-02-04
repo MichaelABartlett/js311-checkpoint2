69742144208a0cd1e6edbbaf960e4b408eca5f2f
DESKTOP-AONL5AH\owner <michaelbartlett864@gmail.com>
2022-02-04 16:37:55 -0600

    pushing changes

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,9 +1,8 @@
 import React from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
-import Work from './components/addRecipe'
-import Navigation from './components/Navigation';
+//import Navigation from './components/Navigation';
 import cookie from 'cookie'
-import Testing from './components/testing';
+//import Testing from './components/testing';
 import LogIn from './components/logIn';
 import Home from './components/home';
 import HowItWorks from './components/howItWorks';
@@ -37,14 +36,12 @@ const Router = () => {
             <Route exact path="/" element={<Home />}/>
             <Route path="/addRecipe" element={ <AddRecipe />}/>
             <Route path="/logIn" element={ <LogIn />} />
-            {/* <Route path="/home" element={<Home />}/> */}
+            <Route path="/home" element={<Home />}/>
             <Route path="/howItWorks" element={<HowItWorks />}/>
             <Route path="/calender" element={<Calender />}/>
             <Route path="/allRecipeList" element={<AllRecipeList />}/>
             <Route path="/pickedRecipe" element={<PickedRecipe />}/>
             <Route path="/createProfile" element={<CreateProfile />}/>
-            {/* <ProtectedRoute path="/admin-view" component={AdminView} /> */}
-            {/* <Route path="/singleListing/:id" component={BizDetails} /> */}
         </Routes>
     );
 };
